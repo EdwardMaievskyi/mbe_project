893a50fdd6f9df5ea6ac922fe0603694fe7e4364
Eduard Maievskyi <e.maievskyi@gmail.com>
2020-12-17 19:17:46 +0100

    Writing of pydocs for the elctronic boards drivers.

--- drivers/valve_control.py
+++ drivers/valve_control.py
@@ -5,44 +5,84 @@ from time import sleep
 class ValveControl:
     def __init__(self, tty_port="/dev/ttyUSB0", baudrate=9600):
         """
-        Array with associated photographic information.
-
+        Tools used for establishing connection with I/O electronic scheme and
+        further controlling of the MBE-setup pneumatic valves.
         ...
 
         Attributes
         ----------
-        tty_port: string
-        System adress 
+        tty_port : string
+            System address of the USB-UART converter
+        baudrate: float
+            Number of pulses per second transmitted via serial bus.
         """
         self.ser = serial.Serial(port=tty_port, baudrate=baudrate, timeout=10,
                                  bytesize=serial.EIGHTBITS,
                                  stopbits=serial.STOPBITS_ONE,
                                  parity=serial.PARITY_NONE,
                                  xonxoff=False, rtscts=False)
-        self.board_adresses = ['0F', '1F']
+        self.board_addresses = ['0F', '1F']
 
     def switch_on_off_valve(self, board_no=0, out_no=1, switch_on=True):
         """
-        
+        Switching on and off output channels controlling directly pneumatic
+        valves.
+
+        Parameters
+        ----------
+        board_no : int
+            Indexing number of the electronic board where the command will
+            be sent. May vary from 0 to N-1, where N is a number of electronic
+            boards used for pneumatic valves control in the MBE experimental
+            setup.
+        out_no : int
+            Indexing number of the output on the electronic board. May vary
+            from 1 to 8.
+        Returns
+        -------
+        str
+            "ok" if a command was accepted for execution
+
         """
         if switch_on:
-            command = '@' + self.board_adresses[board_no] + '+' + str(out_no)
+            command = '@' + self.board_addresses[board_no] + '+' + str(out_no)
         else:
-            command = '@' + self.board_adresses[board_no] + '-' + str(out_no)
+            command = '@' + self.board_addresses[board_no] + '-' + str(out_no)
         print(command)
         self.ser.write(str(command).encode())
         sleep(0.2)
         return(self.ser.readline().decode('ascii'))
 
     def status(self, board_no):
-        print('@' + self.board_adresses[board_no] + '??')
-        self.ser.write(str('@' + self.board_adresses[board_no] +
+        """
+        Requesting the status of inputs and outputs of the electronic board.
+
+        Parameters
+        ----------
+        board_no : int
+            Indexing number of the electronic board where the command will
+            be sent. May vary from 0 to N-1, where N is a number of electronic
+            boards used for pneumatic valves control in the MBE experimental
+            setup.
+        Returns
+        -------
+        str
+            String where the first three symbols represent the board physical
+            address and other symbols present the state (HIGH/LOW == True/False
+            == 1/0) of electronic board inputs and outputs.
+
+        """
+        print('@' + self.board_addresses[board_no] + '??')
+        self.ser.write(str('@' + self.board_addresses[board_no] +
                            '??').encode())
         sleep(0.2)
         return(self.ser.readline().decode('ascii'))
 
 
 if __name__ == '__main__':
+    """
+    Some testing script
+    """
     v_control = ValveControl()
     print(v_control.switch_on_off_valve())
     sleep(0.5)
